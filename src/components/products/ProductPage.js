4d114ae7f97abe3f360ebee1d484c498d5e3bcc6
Mohamadjandali <qaisaljandali944@gmail.com>
2021-10-23 15:35:25 +0300

    getting the product data

--- src/components/products/ProductPage.js
+++ src/components/products/ProductPage.js
@@ -1,8 +1,34 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import doAPIRequest from '../../request';
 
 export default class ProductPage extends Component {
+
+    async componentDidMount() {
+        const [data, error] = await doAPIRequest(
+            'http://localhost:4000',
+            'POST',
+            { 'Content-Type': 'application/json' },
+            {
+                query: `
+                    query {
+                        product(id: "${this.props.match.params.productId}") {
+                        id,
+                        name,
+                        inStock
+                        } 
+                    }
+                `
+            }
+        );
+
+        if (error) {
+            return console.log(error)
+        }
+
+        console.log(data);
+    }
+
     render() {
-        console.log(this.props.match);
         return (
             <div>
                 Product
--- src/request.js
+++ src/request.js
@@ -5,6 +5,10 @@ export default async function doAPIRequest(url, method, headers, body) {
           headers,
           body: JSON.stringify(body)
         });
+
+        if (!response.ok) {
+          throw Error('Something went wrong!');
+        }
   
         const data = await response.json();
         return [data, null];
