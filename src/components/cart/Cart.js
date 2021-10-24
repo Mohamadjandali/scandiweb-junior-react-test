f9c5e86ca091d5086c4479649cbaa3a455a810fa
Mohamadjandali <qaisaljandali944@gmail.com>
2021-10-24 20:48:33 +0300

    adding the part item styles

--- src/components/cart/Cart.js
+++ src/components/cart/Cart.js
@@ -1,20 +1,34 @@
 import React, { Component } from 'react';
 import { APIContext } from '../../Context';
+import './cart.css';
 
 export default class Cart extends Component {
   render() {
     return (
-      <ul>
+      <ul className="cart-list">
         <APIContext.Consumer>
           {({ cart }) => {
             return cart.map((item) => (
-              <li>
-                <div>
-                  <h2>{item.name}</h2>
-                  <span>{item.price}</span>
-                  <img src="https://images-na.ssl-images-amazon.com/images/I/510VSJ9mWDL._SL1262_.jpg" />
+              <React.Fragment>
+                <div className="cart-item">
+                  <div className="cart-item-info">
+                    <div>
+                      <h2>{item.name}</h2>
+                      <h2>{item.price}</h2>
+                    </div>
+                  </div>
+                  <div className="cart-count">
+                    <div className="cart-item-counter">
+                      <span className="increment">+</span>
+                      <span className="item-count">2</span>
+                      <span className="decrement">-</span>
+                    </div>
+                    <div>
+                      <img src="https://images-na.ssl-images-amazon.com/images/I/510VSJ9mWDL._SL1262_.jpg" />
+                    </div>
+                  </div>
                 </div>
-              </li>
+              </React.Fragment>
             ));
           }}
         </APIContext.Consumer>
