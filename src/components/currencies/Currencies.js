c278279850ab04710aa4950f557c928c7b691d05
Mohamadjandali <qaisaljandali944@gmail.com>
2021-11-05 16:04:38 +0200

    adding click outside the currencies to close the component

--- src/components/currencies/Currencies.js
+++ src/components/currencies/Currencies.js
@@ -3,10 +3,30 @@ import { APIContext } from '../../Context';
 import './currencies.css';
 
 export default class Currencies extends Component {
+  constructor(props) {
+    super(props);
+    this.handleClickOutside = this.handleClickOutside.bind(this);
+    this.wrapperRef = React.createRef();
+  }
+
+  componentDidMount() {
+    document.addEventListener('mousedown', this.handleClickOutside);
+  }
+
+  componentWillUnmount() {
+    document.removeEventListener('mousedown', this.handleClickOutside);
+  }
+
+  handleClickOutside(event) {
+    if (this.wrapperRef.current.className === 'currencies-list' && !this.wrapperRef.current.contains(event.target)) {
+        this.props.handleCurrencies();
+    }
+  }
+
   render() {
     return (
       <React.Fragment>
-        <ul className={this.props.toggleCurrency ? 'currencies-list' : 'hide'}>
+        <ul ref={this.wrapperRef} className={this.props.toggleCurrency ? 'currencies-list' : 'hide'}>
           <APIContext.Consumer>
             {({ currencies, setCurrency }) => {
               return currencies.map((currency) => (
