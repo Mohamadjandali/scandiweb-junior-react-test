ff9cb1d0f1d4efa9b886dd66c410753e57a1b2c3
Mohamadjandali <qaisaljandali944@gmail.com>
2021-11-22 00:47:06 +0200

    changing the context provider place

--- src/App.js
+++ src/App.js
@@ -6,7 +6,6 @@ import {
   Redirect,
 } from 'react-router-dom';
 import './App.css';
-import ContextProvider from './Context';
 import Navbar from './components/navbar/Navbar';
 import Products from './components/products/Products';
 import Cart from './components/cart/Cart';
@@ -16,15 +15,13 @@ export class App extends Component {
   render() {
     return (
       <Router>
-        <ContextProvider>
-          <Navbar />
-          <Switch>
-            <Route path="/" exact render={() => <Redirect to="/tech" />} />
-            <Route path="/cart" exact component={Cart} />
-            <Route path="/:category" exact component={Products} />
-            <Route path="/:category/:productId" component={ProductPage} />
-          </Switch>
-        </ContextProvider>
+        <Navbar />
+        <Switch>
+          <Route path="/" exact render={() => <Redirect to="/tech" />} />
+          <Route path="/cart" exact component={Cart} />
+          <Route path="/:category" exact component={Products} />
+          <Route path="/:category/:productId" component={ProductPage} />
+        </Switch>
       </Router>
     );
   }
